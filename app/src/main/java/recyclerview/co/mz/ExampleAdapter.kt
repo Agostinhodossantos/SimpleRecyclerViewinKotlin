be7f474b43e5e3a63b93effd7cdef39b5c14f4af
AgostinhoDossantos <agostinhodossantos27@gmail.com>
2021-01-28 00:48:46 -0800

    config example adapter

--- app/src/main/java/recyclerview/co/mz/ExampleAdapter.kt
+++ app/src/main/java/recyclerview/co/mz/ExampleAdapter.kt
@@ -8,20 +8,24 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.example_item.view.*
 
-class ExampleAdapter : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
+class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
-        val itemView = LayoutInflater.from(parent.context)
-        return ExampleViewHolder()
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+        parent, false)
+
+        return ExampleViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = exampleList[position]
+
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = exampleList.size
 
 
     class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
